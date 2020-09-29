51b25948776d648504353b7156e9e7e17fc1759e
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-09-29 05:43:55 +0000

    added strict type for Calendar

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,36 @@
-declare type Time = { hour: number, minute: number };
+declare type Time = { hour: number; minute: number };
 
-declare enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+declare enum Day {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday
+}
 
 declare type TimeRange = {
-  start: Time,
-  end: Time
+  start: Time;
+  end: Time;
 };
 
-declare type Calendar = Array<
+// Thanks to SO
+type ArrayLengthMutationKeys = 'splice' | 'push' | 'pop' | 'shift' | 'unshift';
+type FixedLengthArray<T, L extends number, TObj = [T, ...Array<T>]> = Pick<
+  TObj,
+  Exclude<keyof TObj, ArrayLengthMutationKeys>
+> & {
+  readonly length: L;
+  [I: number]: T;
+  [Symbol.iterator]: () => IterableIterator<T>;
+};
+
+declare type Calendar = FixedLengthArray<
   {
-    day: Day,
-    timeRange: Array<TimeRange>,
-    classes: Array<string>
- }
->
+    day: Day;
+    timeRange: Array<TimeRange>;
+    classes: Array<string>;
+  },
+  7
+>;
