27f84da5c5e7e6a1fbd65153767eb612bbe5b5dc
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-09-30 17:33:10 +0530

    temp fix for enum value unsettable

--- types.ts
+++ types.ts
@@ -10,13 +10,15 @@ export enum Day {
   Saturday = 'Saturday'
 }
 
+type Days = 'Sunday' | 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday';
+
 export type TimeRange = {
   start: Time;
   end: Time;
 };
 
 export type Calendar = Array<{
-  day: Day;
+  day: Day | Days;
   timeRange: Array<TimeRange>;
   classes: Array<string>;
 }>;
